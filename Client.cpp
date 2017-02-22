17320029321a189597cf1257c157241546c27950
Robyn To <rto@usc.edu>
2017-02-22 00:06:22 -0800

    server can be initialized with port num specified from CL; client can connect to server with args specified from CL and send one message

---
+++ Client.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int socketFileDescriptor;
+    struct addrinfo hints, *serverInfo, *p;
+    int status;
+    int numBytes;
+
+    // args
+    char *serverIP, *portNum, *logfileName, *clientName;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if(argc != 9)
+    {
+        cout << "Usage: client -s ServerIP -p portno -l logfile -n myname" << endl;
+        exit(1);
+    }
+    else
+    {
+        int sCounter, pCounter, lCounter, nCounter;
+        for(int i = 0; i < argc; i++)
+        {
+            if(strcmp(argv[i], "-s") == 0)
+            {
+                sCounter = i+1;
+            }
+            else if(strcmp(argv[i], "-p") == 0)
+            {
+                pCounter = i+1;
+            }
+            else if(strcmp(argv[i], "-l") == 0)
+            {
+                lCounter = i+1;
+            }
+            else if(strcmp(argv[i], "-n") == 0)
+            {
+                nCounter = i+1;
+            }
+        }
+
+        serverIP = argv[sCounter];
+        portNum = argv[pCounter];
+        logfileName = argv[lCounter];
+        clientName = argv[nCounter];
+
+        cout << "DEBUG: serverIP located at " << serverIP << " on port " << portNum << " from client " << clientName << " recorded in logfile " << logfileName << endl;
+    }
+
+    if((status = getaddrinfo(serverIP, portNum, &hints, &serverInfo)) != 0)
+    {
+        cout << "DEBUG: setup error with hints and serverInfo" << endl;
+        return 1;
+    }
+
+
+    for(p = serverInfo; p != NULL; p = p->ai_next)
+    {
+        if((socketFileDescriptor = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+        {
+            cout << "DEBUG: error with socketFileDescriptor" << endl;
+            continue;
+        }
+
+        break;
+    }
+
+    if(p == NULL)
+    {
+        cout << "DEBUG: failed to bind socket" << endl;
+        return 2;
+    }
+
+    if((numBytes = sendto(socketFileDescriptor, "Hello server", strlen("Hello server"), 0, p->ai_addr, p->ai_addrlen)) == -1)
+    {
+        cout << "DEBUG: error on sending message" << endl;
+        exit(1);
+    }
+
+    freeaddrinfo(serverInfo);
+
+    cout << "Sent " << numBytes << " bytes to server" << endl;
+    close(socketFileDescriptor);
+
+    return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,118 @@
+
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+//#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+
+using namespace std;
+void *get_in_addr(struct sockaddr* sa)
+{
+	if(sa->sa_family == AF_INET)
+	{
+		return &(((struct sockaddr_in*) sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char* argv[])
+{
+    struct addrinfo hints, *serverInfo, *p;
+    int buffSize = 1024;
+    int numbytes;
+    char* portNum, *logFileName;
+    struct sockaddr_storage client_addr;
+    socklen_t addr_len;
+    int socketFileDescriptor;
+    char buffer[buffSize];
+    char s[INET6_ADDRSTRLEN];
+    int status; // tells us if our setup is valid
+
+    struct sockaddr_in server_addr;
+    socklen_t size;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE; // use own IP
+
+    cout << "Num args: " << argc << endl;
+    if(argc != 5)
+    {
+    	cout << "Usage: server -p portno -l logfile" << endl;
+    	exit(1);
+    }else{
+    	// Correct num args
+    	int portCounter = 0, logCounter = 0;;
+    	for(int i = 0; i < argc; i++)
+    	{
+    		if(strcmp(argv[i], "-p") == 0)
+    		{
+    			portCounter = i+1;
+    		}
+    		else if(strcmp(argv[i], "-l") == 0)
+			{
+				logCounter = i+1;
+			}
+    	}
+    	portNum = argv[portCounter];
+    	cout << "DEBUG: portNum is " << portNum << endl;
+    	logFileName = argv[logCounter];
+    	cout << "DEBUG: logFileName is " << logFileName << endl;
+    }
+
+   if((status = getaddrinfo(NULL, portNum, &hints, &serverInfo)))
+   {
+   	cout << "DEBUG: Break on line 32...getaddrinfo w/ hints and serverInfo" << endl;
+   	return 1;
+   }
+
+   // loop through addrinfo starting at serverinfo and bind to first one
+   for(p = serverInfo; p != NULL; p=p->ai_next)
+   {
+   	if((socketFileDescriptor = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+   	{
+   		cout << "DEBUG: error on socket listener" << endl;
+   		continue;
+   	}
+
+   	if(bind(socketFileDescriptor, p->ai_addr, p->ai_addrlen) == -1)
+   	{
+   		close (socketFileDescriptor);
+   		cout << "DEBUG: error in socket binding" << endl;
+   		continue;
+   	}
+
+   	cout << "DEBUG: successful bind to socket" << endl;
+   	break; // break if success;
+   }
+
+   if(p == NULL)
+   {
+   	cout << "DEBUG: could not bind to socket" << endl;
+   	return 2;
+   }
+
+   freeaddrinfo(serverInfo);
+
+   cout << "DEBUG: Waiting for recvfrom..." << endl;
+   addr_len = sizeof client_addr;
+   if((numbytes = recvfrom(socketFileDescriptor, buffer, buffSize-1, 0, (struct sockaddr*) &client_addr, &addr_len)) == -1)
+   {
+   	cout << "DEBUG: error on recvfrom" << endl;
+   	exit(1);
+   }
+   
+   cout << "recvfrom: " << inet_ntop(client_addr.ss_family, get_in_addr((struct sockaddr *) &client_addr), s, sizeof s) << endl;
+   cout << "-- packet is " << numbytes << " long" << endl;
+   cout << "-- packet contains '" << buffer << "'" << endl;
+
+   close(socketFileDescriptor);
+
+    return 0;
+}
